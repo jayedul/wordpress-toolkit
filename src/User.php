e785a89cec20559771f87fa739ab98c6c95b0ee5
JK <jayedulk@gmail.com>
2024-07-30 16:33:41 +0600

    Dispatcher moved into

---
+++ src/User.php
@@ -0,0 +1,35 @@
+<?php
+namespace SolidieLib;
+
+class User {
+
+	/**
+	 * Validate if a user has required role
+	 *
+	 * @param int          $user_id The user ID to validate rule
+	 * @param string|array $role    The rule to match
+	 * @return bool
+	 */
+	public static function validateRole( $user_id, $role ) {
+
+		if ( empty( $role ) ) {
+			return true;
+		}
+
+		$roles          = is_array( $role ) ? $role : array( $role );
+		$assigned_roles = self::getUserRoles( $user_id );
+
+		return count( array_diff( $roles, $assigned_roles ) ) < count( $roles );
+	}
+
+	/**
+	 * Get user roles by user id
+	 *
+	 * @param int $user_id User ID to get roles of
+	 * @return array
+	 */
+	public static function getUserRoles( $user_id ) {
+		$user_data = get_userdata( $user_id );
+		return ( is_object( $user_data ) && ! empty( $user_data->roles ) ) ? $user_data->roles : array();
+	}
+}
